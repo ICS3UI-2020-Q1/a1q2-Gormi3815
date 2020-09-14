aafb3735be941861d04af37d9cae2cd719deb60a
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-14 15:05:53 +0000

    made it divide with a remainder

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 /**
  *
  * @author 
@@ -10,6 +13,12 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter two numbers you would like to divide on seperate lines");
+    int firstNumber =input.nextInt();
+    int secondNumber =input.nextInt();
+    int finalNumber =firstNumber/secondNumber;
+    int remainderNumber= firstNumber % secondNumber;
+   System.out.println("The number is " + finalNumber +  " with a remainder of " + remainderNumber + ".");
   }
 }
